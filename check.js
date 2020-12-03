a840aa441be9cff171d8328019ab464eea2ed1dd
BaffledCoder <64409503+BaffledCoder@users.noreply.github.com>
2020-12-03 07:49:05 +0530

    Update check.js

--- check.js
+++ check.js
@@ -25,23 +25,13 @@ function check() {
 		clg4="MIT";
 		count=count+1;
 	}
-	var user = firebase.auth().currentUser;
-	  if (user) 
-	  {
-	  	if(count>0)
+	if(count>0)
 		{
-			console.log("qwerty2");
 			document.getElementById("result").innerHTML = "Success comes to those who deserve it. Congratulations!. From the colleges that have opened enrollment, you can apply for these colleges: " + "|" + clg1 + "|" + "|" + clg2 + "|" + clg3 + "|" + clg4 + "|";
 		}
-		else
+	else
 		{
 			document.getElementById("result").innerHTML = " Couldn't found college as per your requirements but do remember this may change with opening of other college application.\nWhat is the point of being alive if you don't at least try to do something remarkable? ";
 		}
- 	 } 
- 	 else 
- 	 {
- 	 	alert("Login First");
-	    window.location.replace("./login.html");
-	  }
 	
 }
